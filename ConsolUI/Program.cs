00072a5b0f4a8095a99730f9590dfae48310945c
sadık menteş <sadikmentes2133@gmail.com>
2021-02-03 23:48:38 +0300

    Add project files.

---
+++ ConsolUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsolUI
+{
+    class Program
+    {
+      //  private static object product;
+
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(5,10))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
